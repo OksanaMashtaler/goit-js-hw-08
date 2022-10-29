31ba4084a7c13d71256b181e860747e5afa5afb5
OksanaMashtaler <mashoksana@gmail.com>
2022-10-29 18:31:37 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const timeKey = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function onPlay(data) {
+  let currentTime = data.seconds;
+
+  localStorage.setItem(timeKey, currentTime);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if (localStorage.getItem(timeKey)) {
+  player.setCurrentTime(localStorage.getItem(timeKey));
+}
